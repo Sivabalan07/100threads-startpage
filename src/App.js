51385578fb4d1cf51708c435a3e9946c669e065b
Sivabalan <sivabalan6469@gmail.com>
2023-11-10 20:24:55 +0530

    frontdesign

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+//import 'bootstrap/dist/css/bootstrap.min.css'
+import Head from './Component/Header.Component';
 
 function App() {
+  const [showLoginModal, setShowLoginModal] = React.useState(true);
+
+  const handleOpenLoginModal = () => {
+    setShowLoginModal(!showLoginModal);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+      <Head/>
+      <div className="container mt-4">
+        <h1>Your Content Goes Here</h1>
+        {/* Add your page content here */}
+        <br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      </div>
+      </div>
+
   );
 }
 
