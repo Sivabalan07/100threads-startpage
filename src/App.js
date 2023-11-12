1cd656ddc00b87d1bd50a3918a1e223f03e54f0a
Sivabalan <sivabalan6469@gmail.com>
2023-11-12 13:05:07 +0530

    overall02

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import Head_navbar from './Component/Header.Component';
 import Foot_bar from './Component/Footer.Component';
-import logo from './logo.svg'
+// import logo from './logo.svg'
 
 
 function App() {
@@ -13,10 +13,14 @@ function App() {
   return (
       <div>
       <Head_navbar/>
-      <div className="container mt-4 App">
+      <div className="container mt-4 App"  >
         <div className='animation-cs' ><p>Coming soon..</p></div>
         {/* Add your page content here */}
-        <img url={logo} alt='100Threads' style={{opacity:0.5,postion:'relative'}}/>
+        <div className='fade-in show'>
+        <div className='bg-img' role="banner" ></div>   
+        </div>
+        {/* <img url={logo} alt='100Threads' style={{opacity:0.5,postion:'relative'}}/> */}
+        
         <p>The best clothing to buy and have a full comfort of our brand products</p>
       </div>
       <Foot_bar/>
--- src/Component/Header.Component.jsx
+++ src/Component/Header.Component.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./Headerfooter.component.css";
-import logo from '../brand.png'
+// import logo from '../../public/brand.png'
+import logo from "../asset/brand-removebg.png";
+
 
 
 const Head_navbar = () => {
   return (
-    <div>
+    <header>
     <nav className="navbar navbar-expand-lg navbar-light fixed-top"style={{background : 'linear-gradient( #f1e7d9,#fff,#fff,#fff,#fff)'}}>
       <div className="container">
-        <a className="navbar-brand" href="#"><img url={logo} alt='100Threads' width={20} height={10}/></a>
+        <a className="navbar-brand" href="#">
+          {/* <img url="/brand.png" alt='100Threads' width={20} height={10}/> */}
+          {/* <img url={logo} alt='100Threads' width={100} height={"auto"} role='image'/> */}
+          <div className='logobg-img'></div>
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="offcanvas offcanvas-start" id="navbarNav" style={{maxWidth : 240}}>
-          <div class="offcanvas-header" style={{maxWidth : 240}}>
-          <h5 class="offcanvas-title"></h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style={{alignItems:"end"}}></button>
+          <div className="offcanvas-header" style={{maxWidth : 240}}>
+          <h5 className="offcanvas-title"></h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" style={{alignItems:"end"}}></button>
           </div>
           <div className="offcanvas-body" style={{maxWidth : 240}}>
             <ul className="navbar-nav ml-auto">
@@ -37,14 +43,14 @@ const Head_navbar = () => {
     </nav>
 
     {/*Contact popup*/}
-    <div class="modal fade" id="ContactstaticBack" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="ContactstaticBackdrop" aria-hidden="true" >
-      <div class="modal-dialog" >
-        <div class="modal-content modalst" >
-          <div class="modal-header">
-            <h5 class="modal-title" id="ContactstaticBackdrop">Contact Information</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div className="modal fade" id="ContactstaticBack" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="ContactstaticBackdrop" aria-hidden="true" >
+      <div className="modal-dialog" >
+        <div className="modal-content modalst" >
+          <div className="modal-header">
+            <h5 className="modal-title" id="ContactstaticBackdrop">Contact Information</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body radialgradient" style={{textAlign:"Left",padding:40}}>
+          <div className="modal-body radialgradient" style={{textAlign:"Left",padding:40}}>
           Trade name: 100 Threads Clothing 
 <br/><br/>
 Email: 100womenthreads@gmail.com
@@ -54,34 +60,34 @@ Physical address:  manathottam Near AVP school, Aathupalayam poonding ring road,
 
 {/* GST NUMBER : 33ECHPA3781F1ZV */}
           </div>
-          {/* <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Understood</button>
+          {/* <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary">Understood</button>
           </div> */}
         </div>
       </div>
     </div>
 
       {/*About popup*/}
-    <div class="modal fade" id="AboutstaticBack" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="AboutstaticBackdrop" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content modalst">
-          <div class="modal-header">
-            <h5 class="modal-title" id="AboutstaticBackdrop">About us</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div className="modal fade" id="AboutstaticBack" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="AboutstaticBackdrop" aria-hidden="true">
+      <div className="modal-dialog">
+        <div className="modal-content modalst">
+          <div className="modal-header">
+            <h5 className="modal-title" id="AboutstaticBackdrop">About us</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body radialgradient" style={{textAlign:"Left",padding:40}}>
+          <div className="modal-body radialgradient" style={{textAlign:"Left",padding:40}}>
             
           </div>
-          {/* <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Understood</button>
+          {/* <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-primary">Understood</button>
           </div> */}
         </div>
       </div>
     </div>
 
-    </div>
+    </header>
     
   );
 }
