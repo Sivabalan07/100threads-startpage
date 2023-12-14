a6a5f05d4389a11659843b4bc25dcef7b4d92956
nandha <nandhakumars@saptanglabs.com>
2023-12-14 23:13:07 +0530

    front

--- src/Component/Policy/privacy.component.jsx
+++ src/Component/Policy/privacy.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import HeadNavbar from "../Header.Component";
+// import Product from '../Product/product.component';
 
-const Privacy =() =>{
-    return (<HeadNavbar />);
+const Privacy = () => {
+    return (<HeadNavbar>
+       
+    </HeadNavbar>);
 }
 export default Privacy;
