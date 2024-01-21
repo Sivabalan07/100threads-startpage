3a888233a85f53549a3c717184061b16ed5c9078
Sivabalan07 <121504608+Sivabalan07@users.noreply.github.com>
2024-01-21 13:35:42 +0530

    Update Productdata.js | changes

--- src/Function/Productdata.js
+++ src/Function/Productdata.js
@@ -1,2 +1,20 @@
-import data from "./Products.json"
+import productdata from "./Products.json"
+import categorydata from "./CategoryId.json"
 
+let data = productdata;
+const products=()=>{
+    return data
+}
+const category=(name)=>{
+    var menId= categorydata.men.filter(x=>categorydata[name].includes(x));
+    var womenId=categorydata.women.filter(x=>categorydata[name].includes(x));
+    console.log(menId,womenId);
+    var list={
+        men:[],
+        women:[]
+    }
+    menId.forEach(a=>list.men.push(data.filter(x=>x.id.localeCompare(a))));
+    womenId.forEach(a=>list.women.push(data.filter(x=>x.id.localeCompare(a))));
+    return list
+}
+export {products,category}
